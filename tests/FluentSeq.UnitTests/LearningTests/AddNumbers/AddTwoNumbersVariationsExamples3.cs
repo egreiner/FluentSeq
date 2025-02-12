b6a84b0ff194c3c892fca2378647443113a5daa0
egreiner <github.ieg@ieg-tools.net>
2025-02-12 16:13:50 +0100

    wip learning tests

--- tests/FluentSeq.UnitTests/LearningTests/AddNumbers/AddTwoNumbersVariationsExamples3.cs
+++ tests/FluentSeq.UnitTests/LearningTests/AddNumbers/AddTwoNumbersVariationsExamples3.cs
@@ -1,7 +1,5 @@
 ﻿namespace FluentSeq.UnitTests.LearningTests.AddNumbers;
 
-using System.Linq.Expressions;
-
 public sealed class AddTwoNumbersVariationsExamples3
 {
     [Fact]
@@ -17,6 +15,42 @@ public sealed class AddTwoNumbersVariationsExamples3
     public void AddTwoNumbers_ShouldReturn_TheSum_use_lambda_expression2()
     {
         var addFct = (int n1, int n2) => n1 + n2;
+        var actual = addFct.Invoke(1, 2);
+        // or var actual = addFct(1, 2);
+
+        actual.ShouldBe(3);
+    }
+
+
+    [Fact]
+    public void AddTwoNumbers_ShouldReturn_TheSum_use_old_fashioned_delegate()
+    {
+        var addDelegate = new PerformCalculation(Add);
+        var actual = addDelegate.Invoke(1, 2);
+
+        actual.ShouldBe(3);
+    }
+
+    private delegate int PerformCalculation(int x, int y);
+    private int Add(int x, int y) => x + y;
+
+
+
+
+    [Fact]
+    public void AddTwoNumbers_ShouldReturn_TheSum_use_lambda_expression3()
+    {
+        Func<int, int, int> addFct = (x, y) => Add(x, y);
+        var actual = addFct.Invoke(1, 2);
+        // or var actual = addFct(1, 2);
+
+        actual.ShouldBe(3);
+    }
+
+    [Fact]
+    public void AddTwoNumbers_ShouldReturn_TheSum_use_lambda_expression4()
+    {
+        Func<int, int, int> addFct = Add;
         var actual = addFct.Invoke(1, 2);
         // or var actual = addFct(1, 2);
 
--- tests/FluentSeq.UnitTests/LearningTests/AddNumbers/AddTwoNumbersVariationsExamples5.cs
+++ tests/FluentSeq.UnitTests/LearningTests/AddNumbers/AddTwoNumbersVariationsExamples5.cs
@@ -5,62 +5,27 @@ using System.Linq.Expressions;
 public sealed class AddTwoNumbersVariationsExamples5
 {
     [Fact]
-    public void AddTwoNumbers_ShouldReturn_TheSum_use_lambda_expression1()
-    {
-        var addFct = () => 1 + 2;
-        var actual = addFct.Invoke();
-
-        actual.ShouldBe(3);
-    }
-
-
-
-    [Fact]
-    public void AddTwoNumbers_ShouldReturn_TheSum_use_lambda_expression2()
-    {
-        var addFct = (int n1, int n2) => n1 + n2;
-        var actual = addFct.Invoke(1, 2);
-        // or var actual = addFct(1, 2);
-
-        actual.ShouldBe(3);
-    }
-
-
-    [Fact]
-    public void AddTwoNumbers_ShouldReturn_TheSum_use_LINQ()
-    {
-        var numberArray = new [] { 1, 2 };
-
-        var actual = numberArray.Sum();
-
-        actual.ShouldBe(3);
-    }
-
-
-
-    [Fact]
     public void AddTwoNumbers_ShouldReturn_TheSum_use_expression_trees1()
-    {
-        var expression = Expression.Lambda<Func<int>>(Expression.Add(Expression.Constant(1), Expression.Constant(2)));
-        var compiledExpression = expression.Compile();
-
-        var actual = compiledExpression();
-
-        actual.ShouldBe(3);
-    }
-
-
-    [Fact]
-    public void AddTwoNumbers_ShouldReturn_TheSum_use_expression_trees2()
     {
         Func<int> addFct = () => 1 + 2;
         var addFctExpression = Expression.Invoke(Expression.Constant(addFct));
         var compiledExpression = Expression.Lambda<Func<int>>(addFctExpression).Compile();
 
-        var actual = compiledExpression();
+        var actual = compiledExpression.Invoke();
+
+        actual.ShouldBe(3);
+    }
+
+    [Fact]
+    public void AddTwoNumbers_ShouldReturn_TheSum_use_expression_trees2()
+    {
+        var expression = Expression.Lambda<Func<int>>(
+            Expression.Add(Expression.Constant(1), Expression.Constant(2)));
+
+        var compiledExpression = expression.Compile();
+
+        var actual = compiledExpression.Invoke();
 
         actual.ShouldBe(3);
     }
 }
-
-
