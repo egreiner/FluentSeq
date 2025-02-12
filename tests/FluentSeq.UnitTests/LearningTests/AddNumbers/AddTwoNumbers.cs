8272c68bf029a2d116d226b3c5c2eb70537d3461
egreiner <github.ieg@ieg-tools.net>
2025-02-12 14:42:40 +0100

    add Learning Tests

---
+++ tests/FluentSeq.UnitTests/LearningTests/AddNumbers/AddTwoNumbers.cs
@@ -0,0 +1,15 @@
+﻿namespace FluentSeq.UnitTests.LearningTests.AddNumbers;
+
+public class AddTwoNumbers
+{
+    [Fact]
+    public void AddTwoNumbers_ShouldReturn_TheSum()
+    {
+        var number1 = 1;
+        var number2 = 2;
+
+        var actual = number1 + number2;
+
+        actual.ShouldBe(3);
+    }
+}
