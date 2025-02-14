bb6ce82fd11b98f43dee7c2c5dae44ac4a402ceb
egreiner <github.ieg@ieg-tools.net>
2025-02-14 16:16:50 +0100

    add TriggerBuilderForEnumStatesTests.cs

---
+++ tests/FluentSeq.UnitTests/FindTheArchitecture/Spike3/TriggerBuilderForEnumStatesTests.cs
@@ -0,0 +1,53 @@
+namespace FluentSeq.UnitTests.FindTheArchitecture.Spike3;
+
+using FluentSeq.FindTheArchitecture.Spike3;
+
+public sealed class TriggerBuilderForEnumStatesTests
+{
+    [Fact]
+    public void StateBuilder_TriggeredBy_ShouldReturn_TriggerBuilder()
+    {
+        var actual = new FluentSeq<TestState>().Create(TestState.Init)
+            .ConfigureState(TestState.State1)
+            .TriggeredBy(() => true);
+
+        actual.ShouldBeOfType<TriggerBuilder<TestState>>();
+    }
+
+    [Fact]
+    public void WhenInState_ShouldCreate_one_element()
+    {
+        var trigger = new FluentSeq<TestState>().Create(TestState.Init)
+                .ConfigureState(TestState.State1)
+                .TriggeredBy(() => true)
+                .WhenInState(TestState.Bla);
+
+        var actual = trigger.Trigger.WhenInStates;
+
+        actual.ShouldHaveSingleItem();
+    }
+
+    [Fact]
+    public void WhenInStates_ShouldCreate_four_elements()
+    {
+        var trigger = new FluentSeq<TestState>().Create(TestState.Init)
+                .ConfigureState(TestState.State1)
+                .TriggeredBy(() => true)
+                .WhenInStates(TestState.Bla, TestState.Blub, TestState.Fizz, TestState.Buzz);
+
+        var actual = trigger.Trigger.WhenInStates;
+
+        actual.Count.ShouldBe(4);
+    }
+}
+
+public enum TestState
+{
+    Init = 0,
+    State1,
+    State2,
+    Bla,
+    Blub,
+    Fizz,
+    Buzz
+}
