bc48e85a3b66bb2c54d5976e6301d9fe0336e700
egreiner <github.ieg@ieg-tools.net>
2025-02-15 17:50:24 +0100

    add SequenceBuilderExtensions.cs

---
+++ src/FluentSeq/Extensions/SequenceBuilderExtensions.cs
@@ -0,0 +1,18 @@
+﻿namespace FluentSeq.Extensions;
+
+using Builder;
+
+/// <summary>
+/// ExtensionMethods for the SequenceBuilder.
+/// </summary>
+public static class SequenceBuilderExtensions
+{
+    /// <summary>
+    /// Returns true if the state should be validated.
+    /// </summary>
+    /// <param name="builder">The sequence builder</param>
+    /// <param name="state">The specified state</param>
+    public static bool StateShouldBeValidated<TState>(this ISequenceBuilder<TState> builder , TState state) =>
+        !builder.Options.DisableValidation &&
+        !builder.Options.DisableValidationForStates.Contains(state);
+}
