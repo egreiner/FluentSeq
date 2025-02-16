8361e3094a6ed929176d790ca581034a074dd3bb
egreiner <github.ieg@ieg-tools.net>
2025-02-16 17:28:31 +0100

    split up SequenceBuilder tests 2/

---
+++ tests/FluentSeq.UnitTests/Builder/SequenceBuilderSetStateTests.cs
@@ -0,0 +1,16 @@
+﻿namespace FluentSeq.UnitTests.Builder;
+
+public sealed class SequenceBuilderSetStateTests
+{
+    [Fact]
+    public void Sequence_SetState_ShouldBe_test()
+    {
+        var sequence = new FluentSeq<string>().Create("INIT")
+            .Build();
+
+        sequence.SetState("TEST");
+
+        var actual = sequence.CurrentState;
+        actual.ShouldBe("TEST");
+    }
+}
