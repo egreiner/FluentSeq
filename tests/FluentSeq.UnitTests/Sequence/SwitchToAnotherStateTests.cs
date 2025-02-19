6a8bca21793f54e0011b5383c73faba4bfd20f86
egreiner <github.ieg@ieg-tools.net>
2025-02-19 10:50:30 +0100

    SwitchToAnotherStateTests clean up

--- tests/FluentSeq.UnitTests/Sequence/SwitchToAnotherStateTests.cs
+++ tests/FluentSeq.UnitTests/Sequence/SwitchToAnotherStateTests.cs
@@ -3,7 +3,7 @@
 public class SwitchToAnotherStateTests
 {
     [Fact]
-    public void TriggeredSeq_ShouldSwitch_to_requested_state()
+    public void TriggeredSeq_ShouldSwitch_to_initialized()
     {
         var state = new DefaultSequenceStates();
 
@@ -12,9 +12,35 @@ public class SwitchToAnotherStateTests
             .ConfigureState(state.Initialized).TriggeredBy(() => true)
             .Build();
 
+        var actual = sequence.CurrentState;
+        sequence.RegisteredStates.Count.ShouldBe(2);
+        actual.ShouldBe(state.Initializing);
+
         sequence.Run();
 
+        actual = sequence.CurrentState;
+        actual.ShouldBe(state.Initialized);
+    }
+
+    [Fact]
+    public void TriggeredSeq_ShouldSwitch_to_first_triggered_state()
+    {
+        var state = new DefaultSequenceStates();
+
+        var sequence = new FluentSeq<string>().Create(state.Initializing)
+            .ConfigureState(state.Initializing).TriggeredBy(() => false)
+            .ConfigureState(state.Initialized).TriggeredBy(() => true)
+            .ConfigureState(state.On).TriggeredBy(() => true)
+            .ConfigureState(state.Off).TriggeredBy(() => true)
+            .Build();
+
         var actual = sequence.CurrentState;
+        sequence.RegisteredStates.Count.ShouldBe(4);
+        actual.ShouldBe(state.Initializing);
+
+        sequence.Run();
+
+        actual = sequence.CurrentState;
         actual.ShouldBe(state.Initialized);
     }
 }
