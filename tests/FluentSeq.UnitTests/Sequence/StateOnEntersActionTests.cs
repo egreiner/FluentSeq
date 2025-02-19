05a30b4812e52e18081bf8d1b02b2b5bf4ed2db3
egreiner <github.ieg@ieg-tools.net>
2025-02-19 16:41:45 +0100

    add test TriggeredSeq_ShouldRaise_OnlyLast_OnEntryAction

--- tests/FluentSeq.UnitTests/Sequence/StateOnEntersActionTests.cs
+++ tests/FluentSeq.UnitTests/Sequence/StateOnEntersActionTests.cs
@@ -10,17 +10,38 @@ public class StateOnEntryActionTests
 
         var sequence = new FluentSeq<string>().Create(state.Initializing)
             .ConfigureState(state.Initializing)
-                .TriggeredBy(() => false)
-                 .OnEntry(() => x = 1)
+            .TriggeredBy(() => false)
+            .OnEntry(() => x = 1)
             .ConfigureState(state.Initialized)
-                .TriggeredBy(() => true)
-                .OnEntry(() => x = 2)
+            .TriggeredBy(() => true)
+            .OnEntry(() => x = 2)
             .Build();
 
-        // x = 0;
         sequence.Run();
 
         sequence.CurrentState.ShouldBe(state.Initialized);
         x.ShouldBe(2);
+    }
+
+    [Fact]
+    public void TriggeredSeq_ShouldRaise_OnlyLast_OnEntryAction()
+    {
+        var x = 1;
+        var state = new DefaultSequenceStates();
+
+        var sequence = new FluentSeq<string>().Create(state.Initializing)
+            .ConfigureState(state.Initializing)
+            .TriggeredBy(() => false)
+            .OnEntry(() => x *= 2)
+            .ConfigureState(state.Initialized)
+            .TriggeredBy(() => true)
+            .OnEntry(() => x *= 3)
+            .OnEntry(() => x *= 4)
+            .Build();
+
+        sequence.Run();
+
+        sequence.CurrentState.ShouldBe(state.Initialized);
+        x.ShouldBe(4);
     }
 }
