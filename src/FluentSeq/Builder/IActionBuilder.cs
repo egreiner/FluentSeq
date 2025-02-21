df8119e8306455ec43842168da1928787a960d96
egreiner <github.ieg@ieg-tools.net>
2025-02-21 09:28:17 +0100

    OnEntry improve comment

--- src/FluentSeq/Builder/IActionBuilder.cs
+++ src/FluentSeq/Builder/IActionBuilder.cs
@@ -7,7 +7,9 @@ namespace FluentSeq.Builder;
 public interface IActionBuilder<TState>
 {
     /// <summary>
-    /// Action that should be executed when the state is entered
+    /// Action that should be executed when the state is entered.
+    /// Multiple actions could be chained.
+    /// The call order is the same as the orders are defined.
     /// </summary>
     /// <param name="action">The action</param>
     IStateBuilder<TState> OnEntry(Action action);
