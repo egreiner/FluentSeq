79855bac426ebbd41d9bc06271f7053ebaf98a79
egreiner <github.ieg@ieg-tools.net>
2025-02-21 09:44:25 +0100

    add core collections

---
+++ src/FluentSeq/Core/Collection.cs
@@ -0,0 +1,14 @@
+﻿namespace FluentSeq.Core;
+
+/// <summary>
+/// A collection of items
+/// </summary>
+/// <typeparam name="T">The item type</typeparam>
+public abstract class BaseCollection<T> : List<T>
+{
+    /// <inheritdoc />
+    protected BaseCollection() : base() { }
+
+    /// <inheritdoc />
+    protected BaseCollection(IEnumerable<T> enumerable): base(enumerable) { }
+}
---
+++ src/FluentSeq/Core/SeqStateCollection.cs
@@ -0,0 +1,14 @@
+﻿namespace FluentSeq.Core;
+
+/// <summary>
+/// A collection of sequence states
+/// </summary>
+/// <typeparam name="TState">The state type</typeparam>
+public class SeqStateCollection<TState> : BaseCollection<SeqState<TState>>
+{
+    /// <inheritdoc />
+    public SeqStateCollection(): base() { }
+
+    /// <inheritdoc />
+    public SeqStateCollection(IEnumerable<SeqState<TState>> enumerable) : base(enumerable) { }
+}
