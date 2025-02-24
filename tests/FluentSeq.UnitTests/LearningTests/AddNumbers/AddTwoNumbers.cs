2e8fccc85e1e4423736847a950b8e2ec5f63465e
egreiner <github.ieg@ieg-tools.net>
2025-02-24 11:13:23 +0100

    AddTwoNumbers add tests

--- tests/FluentSeq.UnitTests/LearningTests/AddNumbers/AddTwoNumbers.cs
+++ tests/FluentSeq.UnitTests/LearningTests/AddNumbers/AddTwoNumbers.cs
@@ -14,4 +14,49 @@ public sealed class AddTwoNumbers
 
         actual.ShouldBe(3);
     }
+
+    // /// <summary>
+    // /// Add one too max int
+    // /// </summary>
+    // [Fact]
+    // public void AddOneToMax_ShouldReturn_Min()
+    // {
+    //     var actual = int.MaxValue + 1;
+    //
+    //     actual.ShouldBe(int.MinValue);
+    // }
+
+
+    /// <summary>
+    /// Add one too max int
+    /// </summary>
+    [Fact]
+    public void Add1ToAlmostMax_ShouldReturn_Min()
+    {
+        var actual = int.MaxValue - 2;
+
+        for (int i = 1; i <= 3; i++)
+        {
+            actual += 1;
+        }
+
+        actual.ShouldBe(int.MinValue);
+    }
+
+
+    // /// <summary>
+    // /// Add one too max byte
+    // /// </summary>
+    // [Fact]
+    // public void Add1ToAlmostByteMax_ShouldReturn_Min()
+    // {
+    //     var actual = byte.MaxValue - 2;
+    //
+    //     for (byte i = 1; i <= 3; i++)
+    //     {
+    //         actual += 1;
+    //     }
+    //
+    //     actual.ShouldBe(byte.MinValue);
+    // }
 }
