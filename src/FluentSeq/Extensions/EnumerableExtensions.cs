9ed7016e7b1454310c6a1080ee6f4564d7335e9c
egreiner <github.ieg@ieg-tools.net>
2025-02-25 12:36:32 +0100

    remove not needed EnumerableExtension methods 1/

--- src/FluentSeq/Extensions/EnumerableExtensions.cs
+++ src/FluentSeq/Extensions/EnumerableExtensions.cs
@@ -1,7 +1,5 @@
 ﻿namespace FluentSeq.Extensions;
 
-using System;
-using System.Collections;
 using System.Collections.Generic;
 using System.Linq;
 using JetBrains.Annotations;
@@ -12,45 +10,11 @@ using JetBrains.Annotations;
 public static class EnumerableExtensions
 {
     /// <summary>
-    /// Clones the specified source
-    /// </summary>
-    /// <typeparam name="T">The Type</typeparam>
-    /// <param name="source">The list source</param>
-    public static List<T> ToClonedList<T>(this IEnumerable<T> source) =>
-        new(source);
-
-
-    /// <summary>
-    /// Returns a joined enumeration as string.
-    /// </summary>
-    /// <typeparam name="T">The Type.</typeparam>
-    /// <param name="source">The list source.</param>
-    /// <param name="separator"></param>
-    public static string ToJoinedString<T>(this IEnumerable<T>? source, string separator = ", ") =>
-        source == null ? string.Empty : string.Join(separator, source);
-
-
-    /// <summary>
     /// Returns TRUE if the enumerable has items.
     /// </summary>
     /// <param name="enumerable">The enumerable</param>
     /// <typeparam name="T">The type of the enumerable</typeparam>
     public static bool HasItems<T>(this IEnumerable<T>? enumerable) => !IsNullOrEmpty(enumerable);
-
-    /// <summary>
-    /// Returns TRUE if the enumerable has items.
-    /// </summary>
-    /// <param name="enumerable">The enumerable</param>
-    public static bool HasItems(this IEnumerable? enumerable) => !IsNullOrEmpty(enumerable);
-
-    // /// <summary>
-    // /// Returns TRUE if NONE of the items complies to the predicate.
-    // /// </summary>
-    // /// <typeparam name="T">The Type.</typeparam>
-    // /// <param name="source">The list source.</param>
-    // /// <param name="predicate">The predicate</param>
-    // public static bool None<T>(this IEnumerable<T>? source, Func<T, bool> predicate) =>
-    //     !source?.Any(predicate) ?? true;
 
     /// <summary>
     /// Returns TRUE if ALL the items complies to the predicate.
@@ -62,23 +26,4 @@ public static class EnumerableExtensions
     public static bool IsNullOrEmpty<T>(
         [AssertionCondition(AssertionConditionType.IS_NOT_NULL)] this IEnumerable<T>? enumerable) =>
         enumerable?.Any() != true;
-
-    /// <summary>
-    /// Returns TRUE if the enumerable is null or has no items.
-    /// </summary>
-    /// <param name="enumerable"></param>
-    /// <returns></returns>
-    [AssertionMethod]
-    public static bool IsNullOrEmpty([AssertionCondition(AssertionConditionType.IS_NOT_NULL)] this IEnumerable? enumerable) =>
-        enumerable == null || !enumerable.GetEnumerator().MoveNext();
-
-
-    /// <summary>
-    /// Returns an unique enumeration.
-    /// </summary>
-    /// <param name="values"></param>
-    /// <typeparam name="T"></typeparam>
-    /// <returns></returns>
-    public static HashSet<T> Unique<T>(this IEnumerable<T> values) =>
-        new(values);
 }
--- tests/FluentSeq.UnitTests/Extensions/CollectionTests/EnumerableExtensionsTests.cs
+++ tests/FluentSeq.UnitTests/Extensions/CollectionTests/EnumerableExtensionsTests.cs
@@ -1,23 +1,10 @@
 namespace FluentSeq.UnitTests.Extensions.CollectionTests;
 
-using System.Collections;
 using System.Collections.Generic;
 using FluentSeq.Extensions;
 
 public class EnumerableExtensionsTests
 {
-    [Fact]
-    public void ToClonedList_ShouldBe_as_original()
-    {
-        var list = new List<string> { "Test1", "Test2" };
-
-        var actual = list.ToClonedList();
-
-        actual.ShouldContain("Test1");
-        actual.ShouldContain("Test2");
-        actual.ShouldNotContain("Test3");
-    }
-
     [Fact]
     public void IEnumerable_IsNullOrEmpty_ShouldBe_true_when_null()
     {
@@ -37,6 +24,18 @@ public class EnumerableExtensionsTests
 
         actual.ShouldBeTrue();
     }
+
+    [Fact]
+    public void List_IsNullOrEmpty_ShouldBe_false_when_not_empty()
+    {
+        var list = new List<int> { 1 };
+
+        var actual = list.IsNullOrEmpty();
+
+        actual.ShouldBeFalse();
+    }
+
+
 
     [Fact]
     public void IEnumerable_HasItems_ShouldBe_false_when_null()
@@ -69,45 +68,23 @@ public class EnumerableExtensionsTests
     }
 
     [Fact]
-    public void Array_HasItems_ShouldBe_false_when_null()
+    public void List_HasItems_ShouldBe_false_when_empty()
     {
-        ArrayList? array = null;
+        var list = new List<int>();
 
-        bool actual = array.HasItems();
+        var actual = list.HasItems();
 
         actual.ShouldBeFalse();
     }
 
     [Fact]
-    public void Array_HasItems_ShouldBe_true()
-    {
-        var array = new ArrayList(2) { "Test" };
-
-        array.HasItems().ShouldBeTrue();
-    }
-
-    [Fact]
-    public void Array_HasItems_ShouldBe_false()
-    {
-        var array = new ArrayList(2);
-
-        array.HasItems().ShouldBeFalse();
-    }
-
-    [Fact]
-    public void List_HasItems_ShouldBe_false_when_empty()
-    {
-        var list = new List<int>();
-
-        list.HasItems().ShouldBeFalse();
-    }
-
-    [Fact]
     public void List_HasItems_ShouldBe_true_when_not_empty()
     {
-        var array = new List<int> { 1 };
+        var list = new List<int> { 1 };
 
-        array.HasItems().ShouldBeTrue();
+        var actual = list.HasItems();
+
+        actual.ShouldBeTrue();
     }
 
     [Fact]
@@ -115,57 +92,8 @@ public class EnumerableExtensionsTests
     {
         List<int>? list = null;
 
-        // ReSharper disable once ExpressionIsAlwaysNull
-        list.HasItems().ShouldBeFalse();
-    }
+        var actual = list.HasItems();
 
-    [Fact]
-    public void Array_IsNullOrEmpty_ShouldBe_false_when_not_empty()
-    {
-        var array = new ArrayList(2) { "Test" };
-
-        array.IsNullOrEmpty().ShouldBeFalse();
-    }
-
-    [Fact]
-    public void Array_IsNullOrEmpty_ShouldBe_true_when_empty()
-    {
-        var array = new ArrayList(2);
-
-        array.IsNullOrEmpty().ShouldBeTrue();
-    }
-
-    [Fact]
-    public void Array_IsNullOrEmpty_ShouldBe_true_when_null()
-    {
-        ArrayList? array = null;
-
-        // ReSharper disable once ExpressionIsAlwaysNull
-        array.IsNullOrEmpty().ShouldBeTrue();
-    }
-
-    [Fact]
-    public void List_IsNullOrEmpty_ShouldBe_true_when_empty()
-    {
-        var list = new List<int>();
-
-        list.IsNullOrEmpty().ShouldBeTrue();
-    }
-
-    [Fact]
-    public void List_IsNullOrEmpty_ShouldBe_false_when_not_empty()
-    {
-        var array = new List<int> { 1 };
-
-        array.IsNullOrEmpty().ShouldBeFalse();
-    }
-
-    [Fact]
-    public void List_IsNullOrEmpty_ShouldBe_true_when_null()
-    {
-        List<int>? list = null;
-
-        // ReSharper disable once ExpressionIsAlwaysNull
-        list.IsNullOrEmpty().ShouldBeTrue();
+        actual.ShouldBeFalse();
     }
 }
