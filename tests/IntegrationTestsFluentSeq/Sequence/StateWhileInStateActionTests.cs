2e24c24b482cec2c0be1632bdc341ff4931e7634
egreiner <github.ieg@ieg-tools.net>
2025-02-25 16:18:16 +0100

    add project IntegrationTestsFluentSeq

---
+++ tests/IntegrationTestsFluentSeq/Sequence/StateWhileInStateActionTests.cs
@@ -0,0 +1,168 @@
+﻿namespace IntegrationTestsFluentSeq.Sequence;
+
+public class StateWhileInStateActionTests
+{
+    [Fact]
+    public void SequenceRun_ShouldRaise_WhileInStateAction_repeatedly()
+    {
+        int x = 0;
+        var state = new DefaultSequenceStates();
+
+        var sequence = new FluentSeq<string>().Create(state.Initializing)
+            .ConfigureState(state.Initializing)
+            .ConfigureState(state.Initialized)
+                .TriggeredBy(() => true)
+                .WhileInState(() => x += 1)
+            .Build();
+
+        for (int i = 0; i < 3; i++)
+        {
+            sequence.Run();
+            sequence.CurrentState.ShouldBe(state.Initialized);
+        }
+
+        x.ShouldBe(3);
+    }
+
+    [Fact]
+    public void SequenceRun_ShouldRaise_all_WhileInStateAction_repeatedly()
+    {
+        int x = 0;
+        var state = new DefaultSequenceStates();
+
+        var sequence = new FluentSeq<string>().Create(state.Initializing)
+            .ConfigureState(state.Initializing)
+            .ConfigureState(state.Initialized)
+                .TriggeredBy(() => true)
+                .WhileInState(() => x += 1)
+                .WhileInState(() => x *= 2)
+            .Build();
+
+        for (int i = 0; i < 3; i++)
+        {
+            sequence.Run();
+            sequence.CurrentState.ShouldBe(state.Initialized);
+        }
+
+        x.ShouldBe(14);
+    }
+
+    [Fact]
+    public void WhileInState_ShouldBe_called_in_the_specified_order1()
+    {
+        var x = 1;
+        var state = new DefaultSequenceStates();
+
+        var sequence = new FluentSeq<string>().Create(state.Initializing)
+            .ConfigureState(state.Initializing)
+            .ConfigureState(state.Initialized)
+                .TriggeredBy(() => true)
+                .WhileInState(() => x += 1)
+                .WhileInState(() => x *= 2)
+            .Build();
+
+        for (int i = 0; i < 3; i++)
+        {
+            sequence.Run();
+            sequence.CurrentState.ShouldBe(state.Initialized);
+        }
+
+        x.ShouldBe(22);
+        x.ShouldNotBe(15);
+    }
+
+
+    [Fact]
+    public void WhileInState_ShouldBe_called_in_the_specified_order2()
+    {
+        var x = 1;
+        var state = new DefaultSequenceStates();
+
+        var sequence = new FluentSeq<string>().Create(state.Initializing)
+            .ConfigureState(state.Initializing)
+            .ConfigureState(state.Initialized)
+                .TriggeredBy(() => true)
+                .WhileInState(() => x *= 2)
+                .WhileInState(() => x += 1)
+            .Build();
+
+        for (int i = 0; i < 3; i++)
+        {
+            sequence.Run();
+            sequence.CurrentState.ShouldBe(state.Initialized);
+        }
+
+        x.ShouldBe(15);
+        x.ShouldNotBe(22);
+    }
+
+    [Fact]
+    public void WhileInStateAction_ShouldBeCalled_after_OnExitAction_repeatedly()
+    {
+        int x = 0;
+        var state = new DefaultSequenceStates();
+
+        var sequence = new FluentSeq<string>().Create(state.Initializing)
+            .ConfigureState(state.Initializing)
+            .OnExit(() => x += 1)
+            .ConfigureState(state.Initialized)
+            .TriggeredBy(() => true)
+            .WhileInState(() => x *= 2)
+            .Build();
+
+        for (int i = 0; i < 3; i++)
+        {
+            sequence.Run();
+            sequence.CurrentState.ShouldBe(state.Initialized);
+        }
+
+        x.ShouldBe(8);
+    }
+
+    [Fact]
+    public void WhileInStateAction_ShouldBeCalled_after_OnEntryAction_repeatedly()
+    {
+        int x = 0;
+        var state = new DefaultSequenceStates();
+
+        var sequence = new FluentSeq<string>().Create(state.Initializing)
+            .ConfigureState(state.Initializing)
+            .ConfigureState(state.Initialized)
+                .TriggeredBy(() => true)
+                .OnEntry(() => x += 1)
+                .WhileInState(() => x *= 2)
+            .Build();
+
+        for (int i = 0; i < 3; i++)
+        {
+            sequence.Run();
+            sequence.CurrentState.ShouldBe(state.Initialized);
+        }
+
+        x.ShouldBe(8);
+    }
+
+
+    [Fact]
+    public void Definition_order_ShouldBe_irrelevant()
+    {
+        int x = 0;
+        var state = new DefaultSequenceStates();
+
+        var sequence = new FluentSeq<string>().Create(state.Initializing)
+            .ConfigureState(state.Initializing)
+            .ConfigureState(state.Initialized)
+                .TriggeredBy(() => true)
+                .WhileInState(() => x *= 2)
+                .OnEntry(() => x += 2)
+            .Build();
+
+        for (int i = 0; i < 3; i++)
+        {
+            sequence.Run();
+            sequence.CurrentState.ShouldBe(state.Initialized);
+        }
+
+        x.ShouldBe(16);
+    }
+}
---
+++ tests/IntegrationTestsFluentSeq/Sequence/SwitchToAnotherStateTests.cs
@@ -0,0 +1,111 @@
+﻿namespace IntegrationTestsFluentSeq.Sequence;
+
+public class SwitchToAnotherStateTests
+{
+    [Fact]
+    public void TriggeredSeq_ShouldSwitch_to_initialized()
+    {
+        var state = new DefaultSequenceStates();
+
+        var sequence = new FluentSeq<string>().Create(state.Initializing)
+            .ConfigureState(state.Initializing).TriggeredBy(() => false)
+            .ConfigureState(state.Initialized).TriggeredBy(() => true)
+            .Build();
+
+        sequence.RegisteredStates.Count.ShouldBe(2);
+        sequence.CurrentState.ShouldBe(state.Initializing);
+
+        sequence.Run();
+
+        sequence.CurrentState.ShouldBe(state.Initialized);
+    }
+
+    [Fact]
+    public void PreviousState_ShouldBe_Initializing_after_new_state_is_set()
+    {
+        var state = new DefaultSequenceStates();
+
+        var sequence = new FluentSeq<string>().Create(state.Initializing)
+            .ConfigureState(state.Initializing).TriggeredBy(() => false)
+            .ConfigureState(state.Initialized).TriggeredBy(() => true)
+            .Build();
+
+        sequence.RegisteredStates.Count.ShouldBe(2);
+        sequence.CurrentState.ShouldBe(state.Initializing);
+
+        sequence.Run();
+
+        sequence.PreviousState.ShouldBe(state.Initializing);
+    }
+
+    [Fact]
+    public void TriggeredSeq_ShouldSwitch_to_first_triggered_state()
+    {
+        var state = new DefaultSequenceStates();
+
+        var sequence = new FluentSeq<string>().Create(state.Initializing)
+            .ConfigureState(state.Initializing).TriggeredBy(() => false)
+            .ConfigureState(state.Initialized).TriggeredBy(() => true)
+            .ConfigureState(state.On).TriggeredBy(() => true)
+            .ConfigureState(state.Off).TriggeredBy(() => true)
+            .Build();
+
+        sequence.RegisteredStates.Count.ShouldBe(4);
+        sequence.CurrentState.ShouldBe(state.Initializing);
+
+        sequence.Run();
+
+        sequence.CurrentState.ShouldBe(state.Initialized);
+    }
+
+    [Fact]
+    public void TriggeredSeq_ShouldNotSwitch_when_incorrect_CurrentState()
+    {
+        var state = new DefaultSequenceStates();
+
+        var sequence = new FluentSeq<string>().Create(state.Initializing)
+            .ConfigureState(state.Initializing).TriggeredBy(() => false)
+            .ConfigureState(state.Initialized)
+            .ConfigureState(state.On).TriggeredBy(() => true).WhenInState(state.Initialized)
+            .Build();
+
+        sequence.Run();
+
+        sequence.CurrentState.ShouldBe(state.Initializing);
+    }
+
+    [Fact]
+    public void TriggeredSeq_ShouldSwitch_when_correct_CurrentState()
+    {
+        var state = new DefaultSequenceStates();
+
+        var sequence = new FluentSeq<string>().Create(state.Initializing)
+            .ConfigureState(state.Initializing).TriggeredBy(() => false)
+            .ConfigureState(state.Initialized)
+            .ConfigureState(state.On).TriggeredBy(() => true).WhenInState(state.Initialized)
+            .Build();
+
+        sequence.SetState(state.Initialized);
+        sequence.Run();
+
+        sequence.CurrentState.ShouldBe(state.On);
+    }
+
+    [Fact]
+    public void TriggeredSeq_ShouldSwitch_when_correct_CurrentStates()
+    {
+        var state = new DefaultSequenceStates();
+
+        var sequence = new FluentSeq<string>().Create(state.Initializing)
+            .ConfigureState(state.Initializing).TriggeredBy(() => false)
+            .ConfigureState(state.Initialized)
+            .ConfigureState(state.Off)
+            .ConfigureState(state.On).TriggeredBy(() => true).WhenInStates(state.Initialized, state.Off)
+            .Build();
+
+        sequence.SetState(state.Off);
+        sequence.Run();
+
+        sequence.CurrentState.ShouldBe(state.On);
+    }
+}
