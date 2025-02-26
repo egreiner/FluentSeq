15eb44339b8d427b7fa8581290e796c941a8a08b
egreiner <github.ieg@ieg-tools.net>
2025-02-26 17:06:30 +0100

    OnTimerExampleTests add tests

--- tests/IntegrationTestsFluentSeq/Examples/OnTimerExampleTests.cs
+++ tests/IntegrationTestsFluentSeq/Examples/OnTimerExampleTests.cs
@@ -23,10 +23,18 @@ public class OnTimerExampleTests
     [InlineData(false, 0, TimerState.Off, TimerState.Off)]
     [InlineData(false, 0, TimerState.Pending, TimerState.Off)]
     [InlineData(false, 0, TimerState.On, TimerState.Off)]
-    [InlineData(true,  0, TimerState.Off, TimerState.Pending)]
-    [InlineData(true,  1, TimerState.Pending, TimerState.Pending)]
-    [InlineData(true,  51, TimerState.Pending, TimerState.On)]
-    [InlineData(true,  0, TimerState.On, TimerState.On)]
+
+    [InlineData(false, 51, TimerState.Off, TimerState.Off)]
+    [InlineData(false, 51, TimerState.Pending, TimerState.Off)]
+    [InlineData(false, 51, TimerState.On, TimerState.Off)]
+
+    [InlineData(true, 0, TimerState.Off, TimerState.Pending)]
+    [InlineData(true, 0, TimerState.Pending, TimerState.Pending)]
+    [InlineData(true, 0, TimerState.On, TimerState.On)]
+
+    [InlineData(true, 51, TimerState.Off, TimerState.Pending)]
+    [InlineData(true, 51, TimerState.Pending, TimerState.On)]
+    [InlineData(true, 51, TimerState.On, TimerState.On)]
     public async Task Example_Usage_OnTimerConfiguration_Run_async_bla(bool timerInput, int sleepTimeInMs, TimerState currentState, TimerState expectedState)
     {
         _sequence     = GetOnTimerConfiguration().Build();
