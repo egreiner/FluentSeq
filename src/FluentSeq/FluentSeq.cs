488239732d54f779f0837844e341f44d65cf77d2
egreiner <github.ieg@ieg-tools.net>
2025-02-27 10:50:00 +0100

    enhance FluentSeq with configuration method and add OnTimerConfigureExampleTests

--- src/FluentSeq/FluentSeq.cs
+++ src/FluentSeq/FluentSeq.cs
@@ -3,18 +3,30 @@ namespace FluentSeq;
 using Builder;
 
 /// <summary>
-/// This is the root element for this library
+/// This is the root element for FluentSeq
 /// </summary>
 /// <typeparam name="TState">The type of the state</typeparam>
 public class FluentSeq<TState>
 {
     /// <summary>
-    /// Creates a new sequence builder with wich you can configure the state machine
+    /// Initializes a new sequence builder with the specified initial state.
     /// </summary>
-    /// <param name="initialState">The initial state</param>
-    /// <returns>A sequence builder</returns>
+    /// <param name="initialState">The initial state of the sequence</param>
+    /// <returns>A sequence builder to configure the state machine</returns>
     public ISequenceBuilder<TState> Create(TState initialState) =>
         new SequenceBuilder<TState>(initialState);
 
-    // public ISequenceBuilder<TState> Configure(TState initialState)
+
+    /// <summary>
+    /// Configures a sequence with detailed settings using the provided actions.
+    /// </summary>
+    /// <param name="initialState">The initial state of the sequence</param>
+    /// <param name="configurationActions">The actions to configure the sequence</param>
+    /// <returns>A sequence builder with the applied configurations</returns>
+    public ISequenceBuilder<TState> Configure(TState initialState, Action<ISequenceBuilder<TState>> configurationActions)
+    {
+        var sequenceBuilder = Create(initialState);
+        configurationActions.Invoke(sequenceBuilder);
+        return sequenceBuilder;
+    }
 }
---
+++ tests/IntegrationTestsFluentSeq/Examples/OnTimerConfigureExampleTests.cs
@@ -0,0 +1,62 @@
+﻿namespace IntegrationTestsFluentSeq.Examples;
+
+using FluentSeq.Builder;
+
+public class OnTimerConfigureExampleTests
+{
+    private ISequence<TimerState>? _sequence;
+    private bool _onTimerInput;
+
+
+    private ISequenceBuilder<TimerState> GetOnTimerConfiguration()
+    {
+        return new FluentSeq<TimerState>().Configure(TimerState.Off, builder =>
+        {
+            builder.DisableValidationForStates(TimerState.On);
+
+            builder.ConfigureState(TimerState.Off)
+                .TriggeredBy(() => !_onTimerInput);
+
+            builder.ConfigureState(TimerState.Pending)
+                .TriggeredBy(() => _onTimerInput).WhenInState(TimerState.Off);
+
+            builder.ConfigureState(TimerState.On)
+                .TriggeredBy(() => _onTimerInput && currentStateElapsed())
+                .WhenInState(TimerState.Pending);
+
+            return;
+
+            bool currentStateElapsed() => _sequence?.CurrentStateElapsed(TimeSpan.FromMilliseconds(50)) ?? false;
+        }).Builder();
+    }
+
+    [Theory]
+    [InlineData(false, 0, TimerState.Off, TimerState.Off)]
+    [InlineData(false, 0, TimerState.Pending, TimerState.Off)]
+    [InlineData(false, 0, TimerState.On, TimerState.Off)]
+
+    [InlineData(false, 51, TimerState.Off, TimerState.Off)]
+    [InlineData(false, 51, TimerState.Pending, TimerState.Off)]
+    [InlineData(false, 51, TimerState.On, TimerState.Off)]
+
+    [InlineData(true, 0, TimerState.Off, TimerState.Pending)]
+    [InlineData(true, 0, TimerState.Pending, TimerState.Pending)]
+    [InlineData(true, 0, TimerState.On, TimerState.On)]
+
+    [InlineData(true, 51, TimerState.Off, TimerState.Pending)]
+    [InlineData(true, 51, TimerState.Pending, TimerState.On)]
+    [InlineData(true, 51, TimerState.On, TimerState.On)]
+    public async Task Example_Usage_OnTimerConfiguration_Run_async_bla(bool timerInput, int sleepTimeInMs, TimerState currentState, TimerState expectedState)
+    {
+        _sequence     = GetOnTimerConfiguration().Build();
+        _onTimerInput = timerInput;
+
+        _sequence.SetState(currentState);
+
+        await Task.Delay(sleepTimeInMs);
+        await _sequence.RunAsync();
+
+        var actual = _sequence.CurrentState;
+        actual.ShouldBe(expectedState);
+    }
+}
