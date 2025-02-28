c95b6c46fb66ce30e50b81bcd1e8c696af61455e
egreiner <github.ieg@ieg-tools.net>
2025-02-28 17:42:10 +0100

    add DisableValidationTemporarily method to SequenceBuilderExtensions for test purposes

--- src/FluentSeq/Extensions/SequenceBuilderExtensions.cs
+++ src/FluentSeq/Extensions/SequenceBuilderExtensions.cs
@@ -8,6 +8,16 @@ using Builder;
 public static class SequenceBuilderExtensions
 {
     /// <summary>
+    /// The complete validation of the sequence configuration will be disabled temporarily.
+    /// Use this if you want this for test purpose only.
+    /// Later on you can find the usage of the DisableValidationTemporarily method very easy.
+    /// </summary>
+    /// <param name="builder">The sequence builder</param>
+    public static ISequenceBuilder<TState> DisableValidationTemporarily<TState>(this ISequenceBuilder<TState> builder) =>
+        builder.DisableValidation();
+
+
+    /// <summary>
     /// Returns true if validation for state is required.
     /// </summary>
     /// <param name="builder">The sequence builder</param>
