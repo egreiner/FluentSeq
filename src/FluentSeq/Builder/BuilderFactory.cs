4b6fb4208e88b82a76c3b1aed4a3dfbb18674d1d
egreiner <github.ieg@ieg-tools.net>
2025-03-01 17:41:48 +0100

    feat: add BuilderFactory for creating builders

---
+++ src/FluentSeq/Builder/BuilderFactory.cs
@@ -0,0 +1,25 @@
+﻿namespace FluentSeq.Builder;
+
+/// <summary>
+/// A factory for creating all available builders
+/// </summary>
+public static class BuilderFactory<TState>
+{
+    private static ISequenceBuilder<TState>? _sequenceBuilder;
+
+
+    /// <summary>
+    /// 
+    /// </summary>
+    /// <param name="initialState"></param>
+    /// <typeparam name="TState"></typeparam>
+    /// <returns></returns>
+    public static ISequenceBuilder<TState> Create(TState initialState)
+    {
+        if (_sequenceBuilder is not null)
+            return _sequenceBuilder;
+        
+        _sequenceBuilder = new SequenceBuilder<TState>(initialState);
+        return _sequenceBuilder;
+    }
+}
