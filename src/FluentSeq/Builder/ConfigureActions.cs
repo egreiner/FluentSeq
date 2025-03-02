5989542d6925cc73d5a036c9b611f25357d612b8
egreiner <github.ieg@ieg-tools.net>
2025-03-02 10:17:58 +0100

    feat: add ConfigureActions class for state action configuration

---
+++ src/FluentSeq/Builder/ConfigureActions.cs
@@ -0,0 +1,29 @@
+﻿namespace FluentSeq.Builder;
+
+/// <summary>
+/// A helper class for configuring actions on a state
+/// </summary>
+/// <typeparam name="TState"></typeparam>
+public class ConfigureActions<TState>(IStateBuilder<TState> stateBuilder) : ICanConfigureActions<TState>
+{
+    /// <inheritdoc />
+    public IStateBuilder<TState> OnExit(Action action)
+    {
+        stateBuilder.State.ExitActions.Add(action);
+        return stateBuilder;
+    }
+
+    /// <inheritdoc />
+    public IStateBuilder<TState> OnEntry(Action action)
+    {
+        stateBuilder.State.EntryActions.Add(action);
+        return stateBuilder;
+    }
+
+    /// <inheritdoc />
+    public IStateBuilder<TState> WhileInState(Action action)
+    {
+        stateBuilder.State.WhileInStateActions.Add(action);
+        return stateBuilder;
+    }
+}
