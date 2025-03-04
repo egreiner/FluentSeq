8d3c50f67f3fa6d4fe6b8dbf9ed95c2cd82cdac8
egreiner <github.ieg@ieg-tools.net>
2025-03-04 15:49:48 +0100

    refactor: update OffTimerCreateExample to accept dwell time parameter

--- tests/IntegrationTestsFluentSeq/Examples/OffTimerCreateExampleTests.cs
+++ tests/IntegrationTestsFluentSeq/Examples/OffTimerCreateExampleTests.cs
@@ -9,7 +9,7 @@ public class OffTimerCreateExampleTests
     private bool _onTimerInput;
 
 
-    private ISequenceBuilder<TimerState> GetOffTimerConfiguration() =>
+    private ISequenceBuilder<TimerState> GetOffTimerConfiguration(int dwellTimeInMs) =>
         new FluentSeq<TimerState>().Create(TimerState.Off)
             .ConfigureState(TimerState.On)
                 .TriggeredBy(() => _onTimerInput)
@@ -18,28 +18,28 @@ public class OffTimerCreateExampleTests
                 .WhenInState(TimerState.On)
             .ConfigureState(TimerState.Off)
                 .TriggeredBy(() => !_onTimerInput)
-                .WhenInState(TimerState.Pending, () => TimeSpan.FromMilliseconds(20))
+                .WhenInState(TimerState.Pending, () => TimeSpan.FromMilliseconds(dwellTimeInMs))
             .Builder();
 
     [Theory]
-    [InlineData(true, 0, TimerState.Off, TimerState.On)]
-    [InlineData(true, 0, TimerState.Pending, TimerState.On)]
-    [InlineData(true, 0, TimerState.On, TimerState.On)]
+    [InlineData(true, 9, 0, TimerState.Off, TimerState.On)]
+    [InlineData(true, 9, 0, TimerState.Pending, TimerState.On)]
+    [InlineData(true, 9, 0, TimerState.On, TimerState.On)]
 
-    [InlineData(true, 40, TimerState.Off, TimerState.On)]
-    [InlineData(true, 40, TimerState.Pending, TimerState.On)]
-    [InlineData(true, 40, TimerState.On, TimerState.On)]
+    [InlineData(true, 1, 2, TimerState.Off, TimerState.On)]
+    [InlineData(true, 1, 2, TimerState.Pending, TimerState.On)]
+    [InlineData(true, 1, 2, TimerState.On, TimerState.On)]
 
-    [InlineData(false, 0, TimerState.Off, TimerState.Off)]
-    [InlineData(false, 0, TimerState.Pending, TimerState.Pending)]
-    [InlineData(false, 0, TimerState.On, TimerState.Pending)]
+    [InlineData(false, 9,  0, TimerState.Off, TimerState.Off)]
+    [InlineData(false, 9,  0, TimerState.Pending, TimerState.Pending)]
+    [InlineData(false, 9,  0, TimerState.On, TimerState.Pending)]
 
-    [InlineData(false, 40, TimerState.Off, TimerState.Off)]
-    [InlineData(false, 40, TimerState.Pending, TimerState.Off)]
-    [InlineData(false, 40, TimerState.On, TimerState.Pending)]
-    public async Task Example_Usage_OnTimerConfiguration_Run_async(bool timerInput, int sleepTimeInMs, TimerState currentState, TimerState expectedState)
+    [InlineData(false, 1, 2, TimerState.Off, TimerState.Off)]
+    [InlineData(false, 1, 2, TimerState.Pending, TimerState.Off)]
+    [InlineData(false, 1, 2, TimerState.On, TimerState.Pending)]
+    public async Task Example_Usage_OffTimerConfiguration_Run_async(bool timerInput, int dwellTimeInMs, int sleepTimeInMs, TimerState currentState, TimerState expectedState)
     {
-        _sequence     = GetOffTimerConfiguration().Build();
+        _sequence     = GetOffTimerConfiguration(dwellTimeInMs).Build();
         _onTimerInput = timerInput;
 
         _sequence.SetState(currentState);
