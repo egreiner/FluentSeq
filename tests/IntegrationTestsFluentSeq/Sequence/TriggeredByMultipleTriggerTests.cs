9c1a91adb577f302e669279a1f4e774427b7afcd
egreiner <github.ieg@ieg-tools.net>
2025-03-06 08:42:41 +0100

    test: add integration tests for multiple trigger actions

---
+++ tests/IntegrationTestsFluentSeq/Sequence/TriggeredByMultipleTriggerTests.cs
@@ -0,0 +1,70 @@
+﻿namespace IntegrationTestsFluentSeq.Sequence;
+
+public class TriggeredByMultipleTriggerTests
+{
+    [Theory]
+    [InlineData(1, 2)]
+    [InlineData(2, 0)]
+    public async Task First_TriggeredBy_ShouldTrigger_the_Action(int value, int expected)
+    {
+        var x = 0;
+        var state = new DefaultSequenceStates();
+
+        var sequence = new FluentSeq<string>().Create(state.Initializing)
+            .ConfigureState(state.Initializing).TriggeredBy(() => false)
+            .ConfigureState(state.On)
+                .TriggeredBy(() => value == 1)
+                .TriggeredBy(() => value == 3)
+                .OnEntry(() => x = value * 2)
+            .Builder()
+            .Build();
+
+        await sequence.RunAsync();
+
+        x.ShouldBe(expected);
+    }
+
+    [Theory]
+    [InlineData(1, 0)]
+    [InlineData(2, 4)]
+    public async Task Second_TriggeredBy_ShouldTrigger_the_Action(int value, int expected)
+    {
+        var x = 0;
+        var state = new DefaultSequenceStates();
+
+        var sequence = new FluentSeq<string>().Create(state.Initializing)
+            .ConfigureState(state.Initializing).TriggeredBy(() => false)
+            .ConfigureState(state.On)
+                .TriggeredBy(() => value == 4)
+                .TriggeredBy(() => value == 2)
+                .OnEntry(() => x = value * 2)
+            .Builder()
+            .Build();
+
+        await sequence.RunAsync();
+
+        x.ShouldBe(expected);
+    }
+
+    [Theory]
+    [InlineData(1, 2)]
+    [InlineData(2, 4)]
+    public async Task Both_TriggeredBy_ShouldTrigger_the_Action(int value, int expected)
+    {
+        var x = 0;
+        var state = new DefaultSequenceStates();
+
+        var sequence = new FluentSeq<string>().Create(state.Initializing)
+            .ConfigureState(state.Initializing).TriggeredBy(() => false)
+            .ConfigureState(state.On)
+                .TriggeredBy(() => value == 1)
+                .TriggeredBy(() => value == 2)
+                .OnEntry(() => x = value * 2)
+            .Builder()
+            .Build();
+
+        await sequence.RunAsync();
+
+        x.ShouldBe(expected);
+    }
+}
