25a9862be9b1a791dd68fc9e91beb588e16fccdd
egreiner <github.ieg@ieg-tools.net>
2025-07-16 09:08:05 +0200

    Add unit tests for using enums and primitive types as state types

---
+++ tests/UnitTestsFluentSeq/StateTypes/EnumTests.cs
@@ -0,0 +1,20 @@
+﻿namespace UnitTestsFluentSeq.StateTypes;
+
+public sealed class EnumTests
+{
+    [Fact]
+    public void Use_Enum_as_StateType_ShouldNotThrow()
+    {
+        var action = () => new FluentSeq<MyStateEnum>().Create(MyStateEnum.State1)
+            .DisableValidation()
+            .Build();
+
+        action.ShouldNotThrow();
+    }
+
+    private enum MyStateEnum
+    {
+        State1,
+        State2
+    }
+}
---
+++ tests/UnitTestsFluentSeq/StateTypes/PrimitiveTypeTests.cs
@@ -0,0 +1,35 @@
+﻿namespace UnitTestsFluentSeq.StateTypes;
+
+public sealed class PrimitiveTypeTests
+{
+    [Fact]
+    public void Use_String_as_StateType_ShouldNotThrow()
+    {
+        var action = () => new FluentSeq<string>().Create("INIT")
+            .DisableValidation()
+            .Build();
+
+        action.ShouldNotThrow();
+    }
+
+
+    [Fact]
+    public void Use_Int_as_StateType_ShouldNotThrow()
+    {
+        var action = () => new FluentSeq<int>().Create(1)
+            .DisableValidation()
+            .Build();
+
+        action.ShouldNotThrow();
+    }
+    
+    [Fact]
+    public void Use_Double_as_StateType_ShouldNotThrow()
+    {
+        var action = () => new FluentSeq<double>().Create(1.1)
+            .DisableValidation()
+            .Build();
+
+        action.ShouldNotThrow();
+    }
+}
