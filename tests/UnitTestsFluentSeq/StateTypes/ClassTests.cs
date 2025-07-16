6946c7f0a4732523e248fd1fd619023c27eca4dd
egreiner <github.ieg@ieg-tools.net>
2025-07-16 09:12:39 +0200

    Add unit tests for using classes and structs as state types

---
+++ tests/UnitTestsFluentSeq/StateTypes/ClassTests.cs
@@ -0,0 +1,22 @@
+﻿namespace UnitTestsFluentSeq.StateTypes;
+
+public sealed class ClassTests
+{
+    [Fact]
+    public void Use_Class_as_StateType_ShouldNotThrow()
+    {
+        var action = () => new FluentSeq<MyClass>().Create(new MyClass("Initial State"))
+            .DisableValidation()
+            .Build();
+
+        action.ShouldNotThrow();
+    }
+
+    private class MyClass(string state)
+    {
+        public string State { get; set; } = state;
+
+        /// <inheritdoc />
+        public override string ToString() => State;
+    }
+}
---
+++ tests/UnitTestsFluentSeq/StateTypes/StructTests.cs
@@ -0,0 +1,22 @@
+﻿namespace UnitTestsFluentSeq.StateTypes;
+
+public sealed class StructTests
+{
+    [Fact]
+    public void Use_Struct_as_StateType_ShouldNotThrow()
+    {
+        var action = () => new FluentSeq<MyStruct>().Create(new MyStruct("Initial State"))
+            .DisableValidation()
+            .Build();
+
+        action.ShouldNotThrow();
+    }
+
+    private struct MyStruct(string state)
+    {
+        public string State { get; set; } = state;
+
+        /// <inheritdoc />
+        public override string ToString() => State;
+    }
+}
